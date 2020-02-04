9984dde82f01b91ab5f6852c0f26bfc2c8ebc512
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-04 19:33:21 +0000

    Select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,27 +6,27 @@
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, mimnum = 0, tmp = 0, flag = 0;
+	size_t i, j, mimnum = 0, tmp = 0, flag = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        mimnum = i;
-        flag = 0;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[mimnum])
-            {
-                mimnum = j;
-                flag = 1;
-            }
-        }
-        /*swap*/
-        if (flag)
-        {
-            tmp = array[i];
-            array[i] = array[mimnum];
-            array[mimnum] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		mimnum = i;
+		flag = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[mimnum])
+			{
+				mimnum = j;
+				flag = 1;
+			}
+		}
+		/*swap*/
+		if (flag)
+		{
+			tmp = array[i];
+			array[i] = array[mimnum];
+			array[mimnum] = tmp;
+			print_array(array, size);
+		}
+	}
 }
