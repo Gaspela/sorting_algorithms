415d629c3da4ac494f48447a1e9b587348859a91
Gaspela04 <samirmillanorozco@hotmail.com>
2020-02-06 03:58:17 +0000

    Quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,51 +10,50 @@ void quick_sort(int *array, size_t size)
 
     if (size < 2)
         return;
-
     partition(array, size, 0, limit);
 }
 /**
  * partition - Lomute.
  * @arr: Array.
  * @size: Ssize.
- * @lower_limit: Lower limit.
+ * @lowlimit: Lower limit.
  * @limit: Limit.
 */
-void partition(int *arr, size_t size, int lower_limit, int limit)
+void partition(int *arr, size_t size, int lowlimit, int limit)
 {
     int pivot;
 
-    if (lower_limit < limit)
+    if (lowlimit < limit)
     {
-        pivot = sort(arr, size, lower_limit, limit);
-        partition(arr, size, lower_limit, pivot - 1);
+        pivot = sort(arr, size, lowlimit, limit);
+        partition(arr, size, lowlimit, pivot - 1);
         partition(arr, size, pivot + 1, limit);
     }
 }
 /**
  * sort - Sort.
  * @arr: Array.
- * @lower_limit: lower limit.
+ * @lowlimit: lower limit.
  * @limit: Limit.
  * @size: Size.
 */
-int sort(int *arr, size_t size, int lower_limit, int limit)
+int sort(int *arr, size_t size, int lowlimit, int limit)
 {
-    int pivot = arr[limit], swap_index = lower_limit, i;
+    int pivot = arr[limit], swapcurrent = lowlimit, i;
 
-    for (i = lower_limit; i < limit; i++)
+    for (i = lowlimit; i < limit; i++)
         if (arr[i] < pivot)
         {
-            if (i != swap_index && arr[i] != arr[swap_index])
-                swap(&arr[i], &arr[swap_index], size, arr);
+            if (i != swapcurrent && arr[i] != arr[swapcurrent])
+                swap(&arr[i], &arr[swapcurrent], size, arr);
 
-            swap_index++;
+            swapcurrent++;
         }
 
-    if (limit != swap_index && arr[limit] != arr[swap_index])
-        swap(&arr[limit], &arr[swap_index], size, arr);
+    if (limit != swapcurrent && arr[limit] != arr[swapcurrent])
+        swap(&arr[limit], &arr[swapcurrent], size, arr);
 
-    return (swap_index);
+    return (swapcurrent);
 }
 /**
  * swap - swaps two elements in an array.
@@ -70,6 +69,5 @@ void swap(int *one, int *two, size_t size, int *arr)
     tmp = *one;
     *one = *two;
     *two = tmp;
-
     print_array(arr, size);
 }
